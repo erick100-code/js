d019e92bb00908c4a125492d80f51b06d68db52b
Erick Eduardo Souza Araujo <ericksouza23135@gmail.com>
2025-04-26 13:36:13 -0300

    acordion finalizado

--- meus_ex/ex002/src/javascript/script.js
+++ meus_ex/ex002/src/javascript/script.js
@@ -1,11 +1,8 @@
 const accordions = document.querySelectorAll('div.accordion') 
 
-
 accordions.forEach(accordion => {
     accordion.addEventListener('click', () => {
         const body = accordion.querySelector('div.accordion-body')
         body.classList.toggle('active') 
     })
 })
-
-// 11:06
