79a9a1c66622b41f6e0b12defc65faf336c66b76
Erick Eduardo Souza Araujo <ericksouza23135@gmail.com>
2025-04-26 17:19:13 -0300

    imagem escondendo e aparecendo ao clicar

--- meus_ex/ex002/javascript.js
+++ meus_ex/ex002/javascript.js
@@ -1,1 +1,6 @@
 const imagem = document.querySelector('img#imagem')
+const mostrar = document.getElementById('ocult')
+
+mostrar.addEventListener('click', () => {
+    imagem.classList.toggle('active')
+})
