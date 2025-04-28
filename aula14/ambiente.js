d2228877f2517d8c518d31f4ed719b1cf1c77a61
Erick Eduardo Souza Araujo <ericksouza23135@gmail.com>
2025-04-28 13:13:40 -0300

    estrutura de repetição com teste lógico no inicio

--- aula14/ambiente.js
+++ aula14/ambiente.js
@@ -1,6 +1,19 @@
+console.log('VAI COMEÇAR')
+
+for (var c = 1;c <= 10;c++) {
+    console.log(c)
+}
+
+console.log('FIM')
+
+
+//----------------------------------------------------------------------------
+
+/*
 var c = 1
 
 while (c <= 5) {
     console.log(c)
     c++
 }
+*/
--- aula14/node_2810d68615a0a.tmp.js
+++
@@ -1,6 +0,0 @@
-var c = 1
-
-while (c <= 5) {
-    console.log(c)
-    c+3
-}
