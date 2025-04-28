357d7cd47f370e671af69b5ab652f73a6a9b7833
Erick Eduardo Souza Araujo <ericksouza23135@gmail.com>
2025-04-28 13:36:54 -0300

    exercício finalizado

---
+++ aula14/node_b83fe92c6eb5d.tmp.js
@@ -0,0 +1,5 @@
+var quanto = 10
+
+for (var contador = 100; contador >= 1; contador-quanto) {
+    console.log(contador)
+}
---
+++ aula14/teste001.js
@@ -0,0 +1,8 @@
+var quanto = 10
+
+const valor = 50
+
+//     inicialização       teste lógico      atualização de valores    
+for (var contador = 100; contador >= valor; contador = contador - quanto) {//primero o teste lógico
+    console.log(contador) //segundo a execução do codigo
+}//terceiro a atualização dos valores 
